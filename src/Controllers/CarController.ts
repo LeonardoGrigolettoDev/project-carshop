135d04a7a5127aaa165f20cc1d90e61d960f2103
LeonardoGrigolettoDev <leonardochawder@gmail.com>
2023-04-27 15:49:59 -0300

    realiza o req01

---
+++ src/Controllers/CarController.ts
@@ -0,0 +1,39 @@
+import { NextFunction, Request, Response } from 'express';
+import ICar from '../Interfaces/ICar';
+import CarService from '../Services/CarService';
+
+const HTTP_201 = 201;
+
+export default class CarController {
+  private req: Request;
+  private res: Response;
+  private next: NextFunction;
+  private service: CarService;
+
+  constructor(req: Request, res: Response, next: NextFunction) {
+    this.req = req;
+    this.res = res;
+    this.next = next;
+    this.service = new CarService();
+  }
+
+  public async create() {
+    const car: ICar = {
+      id: this.req.body.id,
+      model: this.req.body.model,
+      year: this.req.body.year,
+      color: this.req.body.color,
+      status: this.req.body.status,
+      buyValue: this.req.body.buyValue,
+      doorsQty: this.req.body.doorsQty,
+      seatsQty: this.req.body.seatsQty,
+    };
+
+    try {
+      const newCar = await this.service.createCar(car);
+      return this.res.status(HTTP_201).json(newCar);
+    } catch (err) {
+      this.next(err);
+    }
+  }
+}
