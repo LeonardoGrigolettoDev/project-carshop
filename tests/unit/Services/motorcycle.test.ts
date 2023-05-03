3d1cc307d91bbd486db07dcb30d9289469f67807
LeonardoGrigolettoDev <leonardochawder@gmail.com>
2023-05-03 14:16:21 -0300

    realiza mais que 80% da camada service

--- tests/unit/Services/motorcycle.test.ts
+++ tests/unit/Services/motorcycle.test.ts
@@ -4,12 +4,13 @@ import { Model } from 'mongoose';
 import IMotorcycle from '../../../src/Interfaces/IMotorcycle';
 import MotorcycleService from '../../../src/Services/MotorcycleService';
 
-const MODEL = 'Honda Cb 600f Hornet';
+const MODEL_HORNET = 'Honda Cb 600f Hornet';
+const MODEL_GS1250 = 'GS1250 BMW';
 
 describe('Realiza testes nos componentes services de Motorcycle.', function () {
   it('Deverá realizar um insert no DB corretamente.', async function () {
     const motorcycleInput: IMotorcycle = {
-      model: MODEL,
+      model: MODEL_HORNET,
       year: 2005,
       color: 'Yellow',
       status: true,
@@ -19,7 +20,7 @@ describe('Realiza testes nos componentes services de Motorcycle.', function () {
     };
     const motorcycleOutput: IMotorcycle = {
       id: '6452919cabbf977fafd0c9af',
-      model: MODEL,
+      model: MODEL_HORNET,
       year: 2005,
       color: 'Yellow',
       status: true,
@@ -34,12 +35,12 @@ describe('Realiza testes nos componentes services de Motorcycle.', function () {
 
     expect(result).to.be.deep.equal(motorcycleOutput);
   });
-  
+
   it('Deverá retornar os documentos corretos no método findAll.', async function () {
     const motorcycleOutput = [
       {
         id: '6452919cabbf977fafd0c9af',
-        model: MODEL,
+        model: MODEL_HORNET,
         year: 2005,
         color: 'Yellow',
         status: true,
@@ -59,7 +60,7 @@ describe('Realiza testes nos componentes services de Motorcycle.', function () {
   it('Deverá retornar os documentos corretos no método findById.', async function () {
     const motorcycleOutput: IMotorcycle = {
       id: '6452919cabbf977fafd0c9af',
-      model: 'Honda Cb 600f Hornet',
+      model: MODEL_HORNET,
       year: 2005,
       color: 'Yellow',
       status: true,
@@ -70,6 +71,54 @@ describe('Realiza testes nos componentes services de Motorcycle.', function () {
     sinon.stub(Model, 'findById').resolves(motorcycleOutput);
     const service = new MotorcycleService();
     const result = await service.findById('6452919cabbf977fafd0c9af');
+
+    expect(result).to.be.deep.equal(motorcycleOutput);
+  });
+
+  it('Deverá retornar o objeto que foi atualizado no método findByIdAndUpdate', async function () {
+    const motorcycleOutput: IMotorcycle = {
+      id: '6452919cabbf977fafd0c9af',
+      model: MODEL_GS1250,
+      year: 2005,
+      color: 'Yellow',
+      status: true,
+      buyValue: 30,
+      category: 'Street',
+      engineCapacity: 600,
+    };
+    const motorcycleBody: IMotorcycle = {
+      model: MODEL_GS1250,
+      year: 2005,
+      color: 'Yellow',
+      status: true,
+      buyValue: 30.0,
+      category: 'Street',
+      engineCapacity: 600,
+    };
+    sinon.stub(Model, 'findByIdAndUpdate').resolves(motorcycleOutput);
+    const service = new MotorcycleService();
+    const result = await service.findByIdAndUpdate(
+      '6452919cabbf977fafd0c9af',
+      motorcycleBody,
+    );
+
+    expect(result).to.be.deep.equal(motorcycleOutput);
+  });
+
+  it('Deverá retornar o objeto que foi excluído corretamente.', async function () {
+    const motorcycleOutput = {
+      id: '6452919cabbf977fafd0c9af',
+      model: 'GS1250 BMW',
+      year: 2005,
+      color: 'Yellow',
+      status: true,
+      buyValue: 30,
+      category: 'Street',
+      engineCapacity: 600,
+    };
+    sinon.stub(Model, 'findByIdAndDelete').resolves(motorcycleOutput);
+    const service = new MotorcycleService();
+    const result = await service.deleteById('6452919cabbf977fafd0c9af');
 
     expect(result).to.be.deep.equal(motorcycleOutput);
   });
